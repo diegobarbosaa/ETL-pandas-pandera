2c26edba4c06739f68256daf423894aae8576ceb
Diego Lima <diegobarbosavl@gmail.com>
2025-04-05 14:33:38 -0300

    Função para carregar dados em diferentes formatos. Subir o pyproject.toml.

--- etl.py
+++ etl.py
@@ -2,16 +2,16 @@ import pandas as pd
 import os
 import glob
 
-def consolidar_csv(pasta='data'):
+def consolidar_csv(path: str) -> pd.DataFrame:
     """
     Lê todos os arquivos CSV na pasta especificada, consolida as informações
     em um único DataFrame e retorna o resultado.
     """
     # 1. Encontrar arquivos CSV na pasta
-    arquivos_csv = glob.glob(os.path.join(pasta, '*.csv'))
+    arquivos_csv = glob.glob(os.path.join(path, '*.csv'))
     
     if not arquivos_csv:
-        raise FileNotFoundError(f"Nenhum arquivo CSV encontrado na pasta '{pasta}'.")
+        raise FileNotFoundError(f"Nenhum arquivo CSV encontrado na pasta '{path}'.")
 
     # 2. Ler todos os CSVs
     colunas = [
@@ -39,6 +39,23 @@ def consolidar_csv(pasta='data'):
     
     return df_consolidado
 
+
+def carregar_dados(df: pd.DataFrame, formatos_saida: list):
+    """
+    Carregar o DataFrame em diferentes formatos de arquivo.
+    """
+    if 'csv' in formatos_saida:
+        df.to_csv('dados_consolidados_empresas.csv', index=False)
+    if 'parquet' in formatos_saida:
+        df.to_parquet('dados_consolidados_empresas.parquet', index=False)
+    if 'json' in formatos_saida:
+        df.to_json('dados_consolidados_empresas.json', orient='records')
+    if 'xlsx' in formatos_saida:
+        df.to_excel('dados_consolidados_empresas.xlsx', index=False)
+
 if __name__ == "__main__":
-    df_final = consolidar_csv()
-    df_final
+    pasta='data'
+    df_consolidado = consolidar_csv(path=pasta)
+    format_saida: list = ['json']
+    carregar_dados(df_consolidado, format_saida)
+    print(f"Dados consolidados e salvos em {format_saida}.")
